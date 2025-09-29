6cb8bc1408c60536d7bed0f92e98f1836b5454d3
hailosproduction <hailosproduction@gmail.com>
2025-09-29 12:00:54 +0100

    Create index.js

---
+++ frontend/src/frontend/src/index.js
@@ -0,0 +1,12 @@
+1|import React from "react";
+2|import ReactDOM from "react-dom/client";
+3|import "@/index.css";
+4|import App from "@/App";
+5|
+6|const root = ReactDOM.createRoot(document.getElementById("root"));
+7|root.render(
+8|  <React.StrictMode>
+9|    <App />
+10|  </React.StrictMode>,
+11|);
+12|
